ce050675fc9b36955eb98091049759d62dd1a2fc
bedirhandogan <bedirhandgn14@gmail.com>
2023-01-18 12:25:58 +0300

    fix: focus on input if input is not disabled

--- src/components/Layout/Typing/index.jsx
+++ src/components/Layout/Typing/index.jsx
@@ -1,13 +1,19 @@
 import './styles.css';
 import Word from "../../Shared/Word";
 import {Context} from "../../../context/WordProvider";
-import {useContext} from "react";
+import {useContext, useRef} from "react";
 
 function Typing() {
     const { words } = useContext(Context);
+    const form = useRef();
+
+    const clickHandler = async () => {
+        const inputs = document.body.querySelectorAll(".word > input");
+        await inputs.forEach(v => (v.disabled === false) && v.focus());
+    }
 
     return (
-        <form className={"typing"}>
+        <form className={"typing"} onClick={clickHandler} ref={form}>
             { words?.map((v, i) => <Word value={v?.word} key={i} isDisable={v?.disable} />) }
         </form>
     );
