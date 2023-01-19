9cc0bfd69c5516d5f24ad361b009e37eeaa4346f
bedirhandogan <bedirhandgn14@gmail.com>
2023-01-19 14:35:50 +0300

    refactor: some updates

--- src/components/Layout/Typing/index.jsx
+++ src/components/Layout/Typing/index.jsx
@@ -3,18 +3,24 @@ import Word from "../../Shared/Word";
 import {Context} from "../../../context/WordProvider";
 import {useCallback, useContext, useEffect, useRef, useState} from "react";
 import {CursorArrowIcon} from "@radix-ui/react-icons";
+import {wordList} from "../../utilities";
 
 function Typing() {
-    const { words } = useContext(Context);
-    const formRef = useRef();
     const [formIncludePath, setFormIncludePath] = useState(true);
+    const { words, setWords } = useContext(Context);
+    const formRef = useRef();
 
-    const clickHandler = useCallback(event => {
+    const clickHandler = useCallback(async event => {
         if (event.composedPath().includes(formRef.current)) {
-            document.body.querySelector(".word > input[aria-disabled='false']").focus();
             setFormIncludePath(true);
-        } else setFormIncludePath(false);
-    }, []);
+            document.body.querySelector(".word > input[aria-disabled='false']").focus();
+
+            await [...formRef.current].forEach(v => v.value = "");
+            if (!formIncludePath) await setWords(wordList());
+        } else {
+            setFormIncludePath(false);
+        }
+    }, [setWords, formIncludePath]);
 
     useEffect(() => {
         document.addEventListener("click", clickHandler);
@@ -24,7 +30,7 @@ function Typing() {
 
     return (
         <form className={`typing ${formIncludePath ? 'active' : ''}`} ref={formRef}>
-            <div className={"hover-text"}><CursorArrowIcon /> Click to write </div>
+            <div className={"hover-text"}><CursorArrowIcon /> Click to write and reload</div>
             { words?.map((v, i) => <Word value={v?.word} key={i} isDisable={v?.disable} />) }
         </form>
     );
