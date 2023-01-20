a896d22fe933210946941b43ea0eb16116079ffc
bedirhandogan <bedirhandgn14@gmail.com>
2023-01-20 19:39:18 +0300

    feat: color, time and game mode selection fields

--- src/components/Layout/Config/index.jsx
+++ src/components/Layout/Config/index.jsx
@@ -1,27 +1,30 @@
 import './styles.css';
-import {GearIcon, PersonIcon} from "@radix-ui/react-icons";
-
-function ConfigItem({tip, icon}) {
-    return (
-        <div className={"config-item"}>
-            {icon}
-            <div className={"tooltip"}>{tip}</div>
-        </div>
-    );
-}
+import {BlendingModeIcon, KeyboardIcon, LapTimerIcon} from "@radix-ui/react-icons";
 
 function Config() {
     return (
         <div className={"config"}>
-            <div className={"config-items"}>
-                <ConfigItem
-                    tip={"1v1"}
-                    icon={<PersonIcon width={20} height={20} color={"grey"} /> }
-                />
-                <ConfigItem
-                    tip={"Setting"}
-                    icon={<GearIcon width={20} height={20} color={"grey"} />}
-                />
+            <div className={"config-item"}>
+                <div className={"tooltip"}> Game Mode</div>
+                <KeyboardIcon width={20} height={20} style={{ color: "var(--text-color-third)"}} />
+            </div>
+
+            <div className={"config-item"}>
+                <div className={"tooltip"}>Time</div>
+                <LapTimerIcon width={20} height={20} style={{ color: "var(--text-color-third)"}} />
+                <div className={"time"}>30s</div>
+            </div>
+
+            <div className={"config-item"}>
+                <div className={"tooltip"}>Colors</div>
+                <BlendingModeIcon width={20} height={20} style={{ color: "var(--text-color-third)"}} />
+                <div className={"color background-color-primary"} />
+                <div className={"color background-color-secondary"} />
+                <div className={"color background-color-third"} />
+                <div className={"color text-color-primary"} />
+                <div className={"color text-color-secondary"} />
+                <div className={"color text-color-third"} />
+                <div className={"color text-color-fourth"} />
             </div>
         </div>
     );
