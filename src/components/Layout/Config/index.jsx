0af919cf92c097e2067b91c4a3c0d7cea0e09224
bedirhandogan <bedirhandgn14@gmail.com>
2023-01-20 21:06:49 +0300

    refactor: some changes

--- src/components/Layout/Config/index.jsx
+++ src/components/Layout/Config/index.jsx
@@ -1,20 +1,9 @@
 import './styles.css';
-import {BlendingModeIcon, KeyboardIcon, LapTimerIcon} from "@radix-ui/react-icons";
+import {BlendingModeIcon, KeyboardIcon, LapTimerIcon, StarIcon} from "@radix-ui/react-icons";
 
 function Config() {
     return (
         <div className={"config"}>
-            <div className={"config-item"}>
-                <div className={"tooltip"}> Game Mode</div>
-                <KeyboardIcon width={20} height={20} style={{ color: "var(--text-color-third)"}} />
-            </div>
-
-            <div className={"config-item"}>
-                <div className={"tooltip"}>Time</div>
-                <LapTimerIcon width={20} height={20} style={{ color: "var(--text-color-third)"}} />
-                <div className={"time"}>30s</div>
-            </div>
-
             <div className={"config-item"}>
                 <div className={"tooltip"}>Colors</div>
                 <BlendingModeIcon width={20} height={20} style={{ color: "var(--text-color-third)"}} />
@@ -25,6 +14,22 @@ function Config() {
                 <div className={"color text-color-secondary"} />
                 <div className={"color text-color-third"} />
                 <div className={"color text-color-fourth"} />
+            </div>
+
+            <div className={"config-item"}>
+                <div className={"tooltip"}>Time</div>
+                <LapTimerIcon width={20} height={20} style={{ color: "var(--text-color-third)"}} />
+                <div className={"time"}>30s</div>
+            </div>
+
+            <div className={"config-item"}>
+                <div className={"tooltip"}> Game Mode</div>
+                <KeyboardIcon width={20} height={20} style={{ color: "var(--text-color-third)"}} />
+            </div>
+
+            <div className={"config-item"}>
+                <div className={"tooltip"}> Leaderboard </div>
+                <StarIcon width={20} height={20} style={{ color: "var(--text-color-third)"}} />
             </div>
         </div>
     );
