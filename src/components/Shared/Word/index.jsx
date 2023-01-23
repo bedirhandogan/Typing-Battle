ebb89e90fee3c54615b294932bdfcfa3c9090689
bedirhandogan <bedirhandgn14@gmail.com>
2023-01-23 14:49:23 +0300

    refactor: some changes

--- src/components/Shared/Word/index.jsx
+++ src/components/Shared/Word/index.jsx
@@ -74,7 +74,7 @@ function Word({value, isDisable}) {
                         });
                 }
             } else {
-                if (/^[a-zA-Z0-9wığüşöçĞÜŞÖÇİ*?=.,$%&#^£@€ßæ|<>!:`';~/½_{(-]*$/.test(event.target.value[letters.length])) {
+                if (event.target.value[letters.length] !== " ") {
                     event.target.value = event.target.value.slice(0, letters.length);
                 }
             }
