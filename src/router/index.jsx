4404b63359eaef1ca996c154e0413429fcb955c9
bedirhandogan <bedirhandgn14@gmail.com>
2023-01-26 18:51:38 +0300

    style: if screen width is small show warning message

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,10 +1,15 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "views/Home";
 import 'assets/styles.css';
+import {IconDeviceDesktop} from "@tabler/icons";
 
 function Router() {
     return (
         <div className={"App"}>
+            <div className={"screen-width-warning"}>
+                <IconDeviceDesktop width={50} height={50} />
+                Your screen size is small a larger screen size is required.
+            </div>
             <BrowserRouter>
                 <Routes>
                     <Route path={"/"} element={<Home />} />
